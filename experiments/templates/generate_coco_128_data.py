487256466f7cc7d2873369f063ccc632afc5acaf
Ishan Rai <ishanrai@Ishans-MacBook-Pro.local>
2026-09-13 21:54:44 -0400

    fix: imports + hydra requirements

--- experiments/templates/generate_coco_128_data.py
+++ experiments/templates/generate_coco_128_data.py
@@ -1,12 +1,14 @@
 import hydra
+from omegaconf import DictConfig
 
-from ...src.data.coco.extract.extractor_v1 import ExtractorV1
+from src.data.coco.extract.extractor_v1 import ExtractorV1
 
-@hydra.main(config_path="../../../../experiments/config/data", config_name="coco128")
-def run(cfg):
-    url = cfg.url
-    identifier = cfg.identifier
-
-    extractor = ExtractorV1(url, identifier)
+@hydra.main(version_base=None, config_path="../config/data", config_name="coco128")
+def run(cfg: DictConfig) -> None:
+    extractor = ExtractorV1(cfg.url, cfg.identifier)
     folder_path = extractor.extract()
     print(f"Data extracted to: {folder_path}")
+
+
+if __name__ == "__main__":
+    run()
